095ea0ab2f4cec17ff82981a556f380babbfca9e
BupTyo3806 <alekseyivashin806@yandex.ru>
2016-06-24 13:43:09 +0300

    Point class

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,18 +10,13 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-
-            Draw(x1, y1, sym1);
+            Point p = new Point();
+            p.x = 5;
+            p.y = 6;
+            p.sym = '^';
+            p.Draw();
             
             Console.ReadLine();
-        }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
         }
     }
 }
